53584be3b45b90a04e592958dcef72a80a599f97
qin <qinqiaomeng@outlook.com>
2023-07-18 11:59:44 -0400

    latest

--- lightweight_filtering/cmake/FindLWF.cmake
+++ lightweight_filtering/cmake/FindLWF.cmake
@@ -1,6 +1,6 @@
 # This file was automatically generated during the installation of the lightweight_filtering library
 # and can be used through cmake to find the corresponding header files. A copy of this
-# file was created in /snap/clion/241/bin/cmake/linux/x64/share/cmake-3.25/Modules (depending on the CMAKE_ROOT variable).
+# file was created in /snap/clion/246/bin/cmake/linux/x64/share/cmake-3.25/Modules (depending on the CMAKE_ROOT variable).
 
 set(LWF_INCLUDE_DIRS
 /usr/local/include/LWF/include
